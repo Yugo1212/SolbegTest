8b315bfa6f8fe57aac921ef8f737813733322789
Adam Kaszyński <Yugo1212@outlook.com>
2022-11-16 14:15:58 +0100

    initial commit

---
+++ solbeg_1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using solbeg_1.Models;
+using System.Diagnostics;
+using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace solbeg_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReverseString(string text)
+        {
+            StringBuilder reversedText = new();
+   
+            for (var i = text.Length; i > 0;)
+            {
+                reversedText.Append(text[i - 1]);
+                i--;
+            }
+
+            return Content($"{reversedText}", "application/json");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });   
+        }
+    }
+}
